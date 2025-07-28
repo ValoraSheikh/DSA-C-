ae5b3e289bce2dac84cc36966d775c2a8527d949
Aman Sheikh <haarishsheikh04@gmail.com>
2025-07-28 22:01:13 +0530

    Started L3 and complete till ternary statement

---
+++ Lecture 3/revise.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  char ch;
+  cout << "Enter ch here: ";
+  cin >> ch;
+
+  if( ch >= 'a' && ch <= 'z'){
+    cout << "fuckin lowercase\n";
+  } else {
+    cout<< "Upper case fucker";
+  }
+}
