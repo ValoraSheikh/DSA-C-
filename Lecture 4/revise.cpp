20fefa21f995c62f6d160c6995cd02ba6b0cbb4f
Aman Sheikh <haarishsheikh04@gmail.com>
2025-07-30 15:35:38 +0530

    Complete floyd's and reverse triangle pattern

--- Lecture 4/revise.cpp
+++ Lecture 4/revise.cpp
@@ -94,7 +94,27 @@ int main(){
   //   cout << "\n";
   // }
 
+  // int n=6;
+  // char ch = 'A';
+  // for(int i=0; i<n; i++){
+  //   for(int j=0; j<i+1; j++){
+  //     cout << ch << " ";
+  //     ch++;
+  //   }
+  //   cout << "\n";
+  // }
 
+  // int n=6;
+  
+  // for(int i=0; i<=n-1; i++){
+  //   char ch = 'A' + i;
+    
+  //   for(int j=i+1; j>0; j--){
+  //     cout << ch-- << " ";
+  //   }
+
+  //   cout << "\n";
+  // }
 
   
 
--- Lecture 4/tempCodeRunnerFile.cpp
+++ Lecture 4/tempCodeRunnerFile.cpp
@@ -1,11 +1,12 @@
-  // int n=6;
-  // int num=1;
 
-  // for(int i=0; i<n; i++){
-  //   for(int j=0; j<i+1; j++){
-  //     cout << num << " ";
-  //   }
-    
-  //   num++;
-  //   cout << "\n";
-  // }
+  char ch = 'A';
+
+  for(int i=0; i<=n-1; i++){
+
+    for(int ch=i+1; j>0; j--){
+      cout << ch << " ";
+      
+    }
+
+    cout << "\n";
+  }
