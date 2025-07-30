3506592ba249ec99457867abf539bf5074a12c0f
Aman Sheikh <haarishsheikh04@gmail.com>
2025-07-30 13:53:10 +0530

    Started Lecture 4 and completed printing square pattern

---
+++ Lecture 4/revise.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  // int n=20;
+  // for(int i=1; i<=n; i++){
+  //   for(int j=1; j<=n; j++){
+  //     cout << "&" << "  ";
+  //   }
+  //   cout << "\n";
+  // }
+
+  // int n=10;
+  // for(int i=1; i<=n; i++){
+  //   char ch = 'A';
+  //   for(int j=1; j<=n; j++){
+  //     cout << ch << "  ";
+  //     ch+=1;
+  //   }
+  //   cout << "\n";
+  // }
+
+  // int n=3;
+  // int num = 1;
+  // for(int i=0; i<n; i++){
+  //   for(int j=0; j<n; j++){
+  //     cout << num << "  ";
+  //     num++;
+  //   }
+  //   cout << "\n";
+  // }
+
+  // char ch='A';
+  // int n = 5;
+  // for(int i=0; i<n; i++){
+  //   for(int j=0; j<n; j++){
+  //     cout << ch << "  ";
+  //     ch++;
+  //   }
+  //   cout << "\n";
+  // }
+
+  
+
+
+
+
+  return 0;
+}
