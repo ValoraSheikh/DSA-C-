64c0e1f529054e4d76a3bc86a55ab079fb3417aa
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-01 21:47:11 +0530

    Started L7 and completed Bitwise operator

---
+++ Lecture 7/code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << (1 & 5) << "\n";
+
+  cout << (1 | 5) << "\n";
+  
+  cout << (4 ^ 8) << "\n";
+  
+  cout << (4 << 8) << "\n";
+  
+  cout << (10 >> 1 ) << "\n";
+
+  
+
+  return 0;
+}
