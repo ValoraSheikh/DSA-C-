b0cce86cf958bbf2fb19859dd6b7b55332030bea
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-02 20:51:25 +0530

    Largest and smallest value in array

--- Lecture 8/code.cpp
+++ Lecture 8/code.cpp
@@ -15,17 +15,35 @@ int main() {
   //   cout << arr1[i] << "\n";
   // }
 
-  int size = 5;
-  int age[size];
+  // int size = 5;
+  // int age[size];
 
-  for(int i=0; i<size; i++){
-    cout << "Enter age array: ";
-    cin >> age[i];
-  }
+  // for(int i=0; i<size; i++){
+  //   cout << "Enter age array: ";
+  //   cin >> age[i];
+  // }
 
-  for(int i=0; i<size; i++){
-    cout << age[i] << "\n";
-  }
+  // for(int i=0; i<size; i++){
+  //   cout << age[i] << "\n";
+  // }
+
+  // double arr1[6] = {12.55, 1.245, 78.23, 4, 45, 0.23};
+  // int index;
+  // double smallest = __DBL_MAX__;
+  // double largest = __DBL_MIN__;
+  // for(int i=0; i<6; i++){
+  //   if (arr1[i] < smallest){
+  //     smallest = arr1[i];
+  //     index = i;
+  //   }
+
+  //   // smallest = min(arr1[i], smallest);
+  //   largest = max(arr1[i], largest);
+  // }
+
+  // cout << "Smallest value is at index  = " << smallest << " & " << index << "\n";
+  
+  // cout << "Largest = " << largest << "\n";
 
   return 0;
 }
