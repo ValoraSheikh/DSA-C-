1d82f8e6931960c86f8b2574374d46b9d3c2ae57
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-02 20:27:33 +0530

    Lops on array

--- Lecture 8/code.cpp
+++ Lecture 8/code.cpp
@@ -2,10 +2,30 @@
 using namespace std;
 
 int main() {
-  int arr[5] = {12, 45, 7};
-  double arr1[] = {12.55, 1.245, 78.23};
+  // int arr[5] = {12, 45, 7};
+  // double arr1[] = {12.55, 1.245, 78.23};
+  
+  // cout << arr[-1] << "\n";
+  
+  // double arr1[5] = {12.55, 1.245, 78.23, 4, 45};
+  // int size = sizeof(arr1)/sizeof(double);
+  // cout << sizeof(arr1)/sizeof(double) << "\n";
 
-  cout << arr[-1] << "\n";
+  // for(int i=0; i<size; i++){
+  //   cout << arr1[i] << "\n";
+  // }
+
+  int size = 5;
+  int age[size];
+
+  for(int i=0; i<size; i++){
+    cout << "Enter age array: ";
+    cin >> age[i];
+  }
+
+  for(int i=0; i<size; i++){
+    cout << age[i] << "\n";
+  }
 
   return 0;
 }
