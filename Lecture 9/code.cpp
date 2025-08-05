46288ea3c1166828a1a83a6a41a475d18115cfd2
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-05 09:33:24 +0530

    vector functions

--- Lecture 9/code.cpp
+++ Lecture 9/code.cpp
@@ -4,14 +4,36 @@ using namespace std;
 
 int main(){
   // Vector syntax
-  vector<int> vec = {1, 2, 3};
-  vector<char> vec1(5, 'a');
-  cout << vec[0] << "\n";
-  cout << vec1[0] << "\n";
+  // vector<int> vec = {1, 2, 3};
+  // vector<char> vec1(5, 'a');
+  // cout << vec[0] << "\n";
+  // cout << vec1[0] << "\n";
 
-  for(char val: vec1){
-    cout << val << "\n";
-  }
+  // for(char val: vec1){
+  //   cout << val << "\n";
+  // }
+
+  // vector<int> vec = {1, 2, 3, 5, 8, 9, 7};
+  // cout << "Size = " << vec.size() << "\n";
+
+  // vector<int> vec;
+
+  // cout << "Size = " << vec.size() << "\n";
+  // vec.push_back(78);
+  // vec.push_back(7);
+  // vec.push_back(8);
+  // cout << "Size = " << vec.size() << "\n";
+
+  // vec.pop_back();
+
+  // cout << vec.at(0) << "\n";
+  // cout << vec.front() << "\n";
+  // cout << vec.back() << "\n";
+
+  // for (int i : vec){
+  //   cout << i << "\n";
+  // }
+  
 
   return 0;
 }
