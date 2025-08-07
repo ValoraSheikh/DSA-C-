704c9a3b7829487b6dafe838c39c136deec49767
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-07 20:56:35 +0530

    Optimal brute force approach for pair sum

--- Lecture 12/code.cpp
+++ Lecture 12/code.cpp
@@ -3,23 +3,49 @@
 using namespace std;
 
 int main(){
-  vector<int> vec = {4, 5, 8, 9};
-  int n = vec.size();
-  int target = 17;
-  vector<int> ans;
+  // vector<int> vec = {4, 5, 8, 9};
+  // int n = vec.size();
+  // int target = 17;
+  // vector<int> ans;
 
-  for (int i = 0; i < n; i++){
-    for (int j = i + 1; j < n; j++){
-      if (vec[i] + vec[j] == target){
-        ans.push_back(vec[i]);
-        ans.push_back(vec[j]);
-      }
-    }
-  }
+  // for (int i = 0; i < n; i++){
+  //   for (int j = i + 1; j < n; j++){
+  //     if (vec[i] + vec[j] == target){
+  //       ans.push_back(vec[i]);
+  //       ans.push_back(vec[j]);
+  //     }
+  //   }
+  // }
 
-  for (int val: ans){
-    cout << val << "\n";
-  }
+  // for (int val: ans){
+  //   cout << val << "\n";
+  // }
+
+  // vector<int> vec = {4, 5, 8, 9, 10, 5};
+  // int n = vec.size();
+  // int target = 15;
+  // int pairSum = 0;
+  // vector<int> ans;
+
+  // int i = 0, j = n-1;
+
+  // while (i < j){
+  //   pairSum = vec[i] + vec[j];
+  //   if (pairSum < target){
+  //     i++;
+  //   } else if (pairSum > target){
+  //     j--;
+  //   } else{
+  //     ans.push_back(vec[i]);
+  //     ans.push_back(vec[j]);
+  //     break;
+  //   }
+  // }
+
+  // for (int val: ans){
+  //   cout << val << "\n";
+  // }
+  
 
   return 0;
 }
