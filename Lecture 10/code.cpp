f588f1c0f75e5071cad7a46425bf5d9ed13165cf
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-07 20:27:52 +0530

    Complete Lecture 10 & 11

--- Lecture 10/code.cpp
+++ Lecture 10/code.cpp
@@ -2,18 +2,55 @@
 using namespace std;
 
 int main(){
-  int n = 5;
-  int arr[n] = {1, 2, 3, 4, 5};
 
-  for (int st = 0; st < n; st++){
-    for (int end = st; end < n; end++){
-      for (int i = st; i <= end; i++){
-        cout << arr[i];
-      }
-      cout << " ";
+  // Mine code 😂
+
+  // int n = 5;
+  // int arr[n] = {1, 2, 3, 4, 5};
+  // int subSum = 0;
+
+  // for (int st = 0; st < n; st++){
+  //   for (int end = st; end < n; end++){
+  //     for (int i = st; i <= end; i++){
+  //       cout << arr[i];
+  //       subSum += arr[i];
+  //     }
+  //     cout << " ";
+  //     cout << "Sum is here = " << subSum << "\n";
+  //     subSum = 0;
+  //   }
+  //   cout << endl;
+  // }
+
+  // Teacher code 👩‍🏫
+  
+  // int n = 5;
+  // int arr[n] = {1, 2, 3, 4, 5};
+  // int subSum = INT16_MIN;
+
+  // for (int st = 0; st < n; st++){
+  //   int curSum = 0;
+  //   for (int end = st; end < n; end++){
+  //     curSum += arr[end];
+  //     subSum = max(curSum, subSum);
+  //   }
+  // }
+
+  // cout << "max subarray = " << subSum << "\n";
+  
+
+  int arr[8] = {1, 2, -9, 8, 7, -2, -12, 45};
+  int curSum = 0;
+  int maxSum = INT16_MIN; 
+  for (int i = 0; i < 8; i++){
+    curSum += arr[i];
+    maxSum = max(maxSum, curSum);
+    if (curSum < 0){
+      curSum = 0;
     }
-    cout << endl;
   }
+
+  cout << "MAX SUM = " << maxSum;
   
 
   return 0;
