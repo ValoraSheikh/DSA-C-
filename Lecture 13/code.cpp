fc784f137210fab3130e7919ddf82ef572e23b5f
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-11 11:15:19 +0530

    Started L 13 and compute x^n

---
+++ Lecture 13/code.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  // if (n == 0) return 1;
+  // if (x == 0) return 0.0;
+  // if (x == 1) return 1.0;
+  // if (x == -1 && n%2 == 0) return 1.0;
+  // if (x == -1 && n%2 != 0) return -1.0;
+
+
+  // long binForm = n;
+  // if (n < 0){
+  //   x = 1/x;
+  //   binForm = -binForm;
+  // }
+  
+
+  // double ans = 1;
+
+  // while (binForm > 0){
+  //   if (binForm % 2 == 0){
+  //     ans *= x;
+  //   }
+  //   x *= x;
+  //   binForm /= 2;
+  // }
+  // return ans;
+  
+
+
+    return 0;
+}
