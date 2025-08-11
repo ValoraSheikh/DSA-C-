281455628704582adc733dd584457f84c7629bda
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-11 11:15:33 +0530

    Revise

---
+++ Revise/code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//   int arr[5] = {10, 20, 30, 40, 50};
+// cout << arr[5] << endl;
+
+int arr[5] = {2, 4, 6, 8, 10};
+
+for (int i = 0; i <= 5; i++){
+  cout << arr[i] << " ";
+}
+
+return 0;
+}
