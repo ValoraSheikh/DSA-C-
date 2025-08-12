8f711438feb55ab59c1cb98d4010ef813af28bb4
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-12 21:52:39 +0530

    2 pointer optimal approach to take most water in container

--- Lecture 14/code.cpp
+++ Lecture 14/code.cpp
@@ -2,22 +2,40 @@
 using namespace std;
 
 int main(){
-  int height[6] = {1, 4, 3, 7, 5, 6};
+  // int height[6] = {1, 4, 3, 7, 5, 6};
+
+  // int maxWaterArea = 0;
+
+  // for (int i = 0; i < 6; i++){
+  //   for (int j = i+1; j < 6; j++){
+  //     int width = j - i;
+  //     int height_i = min(height[i], height[j]);
+  //     int waterArea = width * height_i;
+  //     maxWaterArea = max(maxWaterArea, waterArea);
+  //   }
+    
+  // }
+
+  // cout << "Maximum water area: " << maxWaterArea << endl;
+  // cout << "Expected: 20" << endl;
+  
 
   int maxWaterArea = 0;
-
-  for (int i = 0; i < 6; i++){
-    for (int j = i+1; j < 6; j++){
-      int width = j - i;
-      int height_i = min(height[i], height[j]);
-      int waterArea = width * height_i;
-      maxWaterArea = max(maxWaterArea, waterArea);
-    }
+  int lp = 0, rp = height.size() - 1;
+  
+  while (lp < rp) {
+    int width = rp - lp;
+    int height_i = min(height[lp], height[rp]);
+    int waterArea = width * height_i;
+    maxWaterArea = max(maxWaterArea, waterArea);
     
+    if (height[lp] < height[rp]) {
+      lp++;
+    } else {
+      rp--;
+    }
   }
 
-  cout << "Maximum water area: " << maxWaterArea << endl;
-  cout << "Expected: 20" << endl;
-  
+
   return 0;
 }
