1097e2935314e0159640e5999cd1fffbe281a73a
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-19 09:56:48 +0530

    pointer to pointer and Derefrence operator

--- Lecture 16/code.cpp
+++ Lecture 16/code.cpp
@@ -3,18 +3,25 @@ using namespace std;
 
 int main() {
   int a = 12;
-  cout << &a << endl;
+  // cout << &a << endl;
 
   //Pointers
   int *p = &a;
-  cout << p << endl;
-  cout << *p << endl;
-  cout << &p << endl;
-  
+  // cout << p << endl;
+  // cout << *p << endl;
+  // cout << &p << endl;
+
+  cout << "Address of pointer p: " << &p << endl;
+
   float price = 100.48f;
   float *ptr = &price;
   cout << ptr << endl;
   cout << *ptr << endl;
+
+  float **ptr2 = &ptr;
+  cout << "ptr2 = " << ptr2 << endl;
+  cout << *ptr2 << endl;
+  cout << **ptr2 << endl;
 
   return 0;
 }
