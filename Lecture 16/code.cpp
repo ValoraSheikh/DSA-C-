2243bbc21c2e88fe70bf421da02ce63d03cf3323
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-19 09:38:56 +0530

    Pointer in C++

--- Lecture 16/code.cpp
+++ Lecture 16/code.cpp
@@ -5,5 +5,16 @@ int main() {
   int a = 12;
   cout << &a << endl;
 
+  //Pointers
+  int *p = &a;
+  cout << p << endl;
+  cout << *p << endl;
+  cout << &p << endl;
+  
+  float price = 100.48f;
+  float *ptr = &price;
+  cout << ptr << endl;
+  cout << *ptr << endl;
+
   return 0;
 }
