9a8e74077eac296675c9c501357509cdcb0b0df4
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-20 20:35:25 +0530

    Started lecture 17 and code for binary search iterative

---
+++ Lecture 17/code.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearchAlgo(vector<int> arr, int tar){
+  int st = 0, ed = arr.size() - 1;
+
+  while (st <= ed){
+    int mid = (st + ed) / 2;
+
+    if (tar > arr[mid]){
+      st = mid + 1;
+    } else if (tar < arr[mid]){
+      ed = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return -1;
+}
+
+int main() {
+  vector<int> numbers = {1, 2, 3, 4, 5};
+  int target = 5;
+  int result = binarySearchAlgo(numbers, target);
+
+  if (result != -1) {
+    cout << "Element found at index: " << result << endl;
+  } else {
+    cout << "Element not found" << endl;
+  }
+  
+  
+}
